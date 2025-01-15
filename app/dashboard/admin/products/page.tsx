c86b5bb06f5d37d61c7c0984b3071802ae30396f
briozetta <briozettatechnologies@gmail.com>
2025-01-15 22:13:51 +0530

    upping

--- app/dashboard/admin/products/page.tsx
+++ app/dashboard/admin/products/page.tsx
@@ -1,14 +1,14 @@
-"use client"
+
 import  ProductAddForm  from '@/components/helpers/ProductAddForm'
-import DashboardLayout from '@/components/shared/DashboardLayout'
+
 import { ProductList } from '@/components/shared/ProductList'
 
 
 export default function productsPage() {
   return (
-   <DashboardLayout>
+   <>
     <ProductAddForm/>
     <section><ProductList/></section>
-   </DashboardLayout>
+   </>
   )
 }
